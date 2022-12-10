158ea354d819d660f0b3edac6ce4c8cd6356b33b
Olamide Omolola <olamide.omolola@grommunio.com>
2022-12-10 15:42:09 +0100

    removed redis

--- set-up.sh
+++ set-up.sh
@@ -3,14 +3,8 @@
 echo 'This script assumes that you have a working kubernetes cluster, the kubectl context is set to it and helm v3 installed (all latest versions)' 
 
 # Add helm repositories
-helm repo add jitsi https://jitsi-contrib.github.io/jitsi-helm/
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-
-# Setup Grommunio Jitsi
-#helm install \
-#	gromox-meet jitsi/jitsi-meet \
-#	-f video/video-values.yml
 
 # Setup cert-manager
 helm install \
@@ -24,8 +18,6 @@ helm install \
 kubectl apply -f db/db-config.yaml
 kubectl apply -f admin-api/admin-plugins-config.yaml
 kubectl apply -f admin-api/admin-links-config.yaml
-kubectl apply -f redis/redis-config.yaml
-kubectl apply -f redis/redis.yaml
 kubectl apply -f db/db.yaml
 kubectl apply -f certificates/test-certificate.yaml
 kubectl apply -f general/general-config.yaml
